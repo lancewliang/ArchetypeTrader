09550b4699c53bd9bfc040b9f20246b0743e4bd5
lance.liang <78599194@qq.com>
2026-04-24 22:57:43 +0800

    1

---
+++ sh/FU/best.sh
@@ -0,0 +1,5 @@
+./run_pipeline.sh FU batch_03-short-1 \
+  --cycle-feature-sets short \
+  --phase1-codebook-separation-weight 0.08 \
+  --phase1-codebook-separation-margin 0.15 \
+  --phase1-low-usage-reset-threshold 0.03
